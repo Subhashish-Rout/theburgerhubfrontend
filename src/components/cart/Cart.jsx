c24fa1eb755eb3acbc10dcfdb5f99a1c901738df
Subhashish-Rout <routsubhashish2@gmail.com>
2023-02-23 19:54:54 +0530

    final commit

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import burger1 from "../../assests/burger1.png";
 import burger2 from "../../assests/burger2.png";
 import burger3 from "../../assests/burger3.png";
 
-const CartItem = ({ value, title, img, increment, decrement }) => (
+const CartItem = ({ value, title, img, increment, decrement}) => (
   <div className="cartItem">
     <div>
       <h4>{title}</h4>
@@ -20,51 +20,117 @@ const CartItem = ({ value, title, img, increment, decrement }) => (
 );
 
 const Cart = () => {
-  const increment = (item) => {};
+  
+  const[count1,setcount1] = useState(0);
+  const[count2,setcount2] = useState(0);
+  const[count3,setcount3] = useState(0);
+  const[subTotal,setsubTotal] = useState(0);
+  const[tax,setTax] = useState(0);
+  const[shippingCh,setshippingCh] = useState(0);
+  const[totalAmt,setTotalAmt] = useState(0);
+  
+  const increment = () => {
+    setcount1(count1+1)
+    setsubTotal(subTotal+200)
+    setTax(subTotal*0.18)
+    setshippingCh(subTotal*0.1)
+    setTotalAmt(subTotal+tax+shippingCh)
+  };
 
-  const decrement = (item) => {};
+  const decrement = () => {
+    if(count1!==0){
+      setcount1(count1-1)
+      setsubTotal(subTotal-200)
+      setTax(subTotal*0.18)
+      setshippingCh(subTotal*0.1)
+      setTotalAmt(subTotal+tax+shippingCh)
+    }
+    
+    
+  };
+  const increment2 = () => {
+    setcount2(count2+1)
+    setsubTotal(subTotal+500)
+    setTax(subTotal*0.18)
+    setshippingCh(subTotal*0.1)
+    setTotalAmt(subTotal+tax+shippingCh)
+  };
+
+  const decrement2 = () => {
+    if(count2 !== 0){
+      setcount2(count2-1)
+      setsubTotal(subTotal-500)
+      setTax(subTotal*0.18)
+      setshippingCh(subTotal*0.1)
+      setTotalAmt(subTotal+tax+shippingCh)
+    }
+    
+  };
+  const increment3 = () => {
+    setsubTotal(subTotal+1800)
+    setcount3(count3+1)
+    setTax(subTotal*0.18)
+    setshippingCh(subTotal*0.1)
+    setTotalAmt(subTotal+tax+shippingCh)
+  };
+
+  const decrement3 = () => {
+    
+    if(count3 !== 0) {
+      setcount3(count3-1)
+      setsubTotal(subTotal-1800)
+      setTax(subTotal*0.18)
+      setshippingCh(subTotal*0.1)
+      setTotalAmt(subTotal+tax+shippingCh)
+    }
+    
+  };
 
   return (
     <section className="cart">
       <main>
-        <CartItem
+      <CartItem
           title={"Cheese Burger"}
           img={burger1}
-          value={0}
-          increment={() => increment(1)}
-          decrement={() => decrement(1)}
+          value={count1}
+          increment={() => increment()}
+          decrement={() => decrement()}
+          
+
         />
         <CartItem
           title={"Veg Cheese Burger"}
           img={burger2}
-          value={0}
-          increment={() => increment(2)}
-          decrement={() => decrement(2)}
+          value={count2}
+          increment={() => increment2()}
+          decrement={() => decrement2()}
         />
         <CartItem
           title={"Cheese Burger with French Fries"}
           img={burger3}
-          value={0}
-          increment={() => increment(3)}
-          decrement={() => decrement(3)}
+          value={count3}
+          increment={() => increment3()}
+          decrement={() => decrement3()}
         />
 
         <article>
           <div>
             <h4>Sub Total</h4>
-            <p>₹{2000}</p>
+            {/* {subTotal = count1*200+count2*500+count3*1800 } */}
+            <p>₹{subTotal}</p>
           </div>
           <div>
             <h4>Tax</h4>
-            <p>₹{2000 * 0.18}</p>
+            {/* {setTax(subTotal*0.18)} */}
+            <p>₹{tax}</p>
           </div>
           <div>
             <h4>Shipping Charges</h4>
-            <p>₹{200}</p>
+            <p>₹{shippingCh}</p>
           </div>{" "}
           <div>
             <h4>Total</h4>
-            <p>₹{2000 + 2000 * 0.18 + 200}</p>
+            <p>₹{totalAmt}</p>
           </div> 
           <Link to="/shipping">Checkout</Link>
         </article>
